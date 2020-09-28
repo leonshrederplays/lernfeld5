0093ba0a810fd8aa7d717b21ecc5bd1bbb67bb52
Leon <leon.morbacher@engn.de>
2020-09-28 13:15:44 +0200

    Changed connector Method and added an Try Catch

--- src/main/java/utils/DBUtils.java
+++ src/main/java/utils/DBUtils.java
@@ -1,27 +1,48 @@
 package utils;
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.UUID;
 
 public class DBUtils {
     Connection conn = null;
 
-    public void connector() {
-
+    public Connection connector() {
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://51.38.194.126:/test?" + "user=shreder&password=Like1234");
+            String url = "jdbc:mysql://51.38.194.126:3306/test";
+            String user = "shreder";
+            String pass = "like1234";
+            conn = DriverManager.getConnection(url, user, pass);
+            conn.setAutoCommit(false);
             createSQL();
         } catch (SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
         }
+        return conn;
     }
 
     public void createSQL(){
-
+        try (Connection con = connector()) {
+            // Hier SQL Befehl in den String schreiben.
+            String sql = "SELECT FRIENDID FROM FRIENDS WHERE NAME = ?";
+            try (PreparedStatement ps = con.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)) {
+                ps.setObject(1, "Hendrik");
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (!rs.next()) {
+                        System.out.println("No requests");
+                    } else {
+                        do {
+                            // rs.getObject or etc. And Column Number required.
+                            rs.getString(1);
+                        } while (rs.next());
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void recreateSQL(){
