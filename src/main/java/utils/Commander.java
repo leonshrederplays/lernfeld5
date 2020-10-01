370e456ef5adb6331e9d3a23c089722545414ad4
JAKOBS-LAPTOP\jakob <jakob.kamper.recke@googlemail.com>
2020-10-01 11:51:52 +0200

    Added all commands to the "help" command

--- src/main/java/utils/Commander.java
+++ src/main/java/utils/Commander.java
@@ -16,11 +16,16 @@ public class Commander {
     public DBUtils dbUtil = new DBUtils();
 
     public void helper(){
-        System.out.print("Here is a list of available commands\n" +
+        System.out.print("Here is a list of available commands:\n" +
                 "\n" +
                 "help -- shows a list of all available commands\n" +
-                "ingreds / ingreds <ingredient name>/<ingredient ID> -- lists all available ingredients/lists the details of one ingredient\n" +
-                "exit -- exit the program\n\n");
+                "ingreds / ingreds <ingredient name/ingredient ID> -- list all ingredients / lists the details of one igredient\n" +
+                "recipe / recipe <recipe name/recipe ID> -- lists all recipes / lists the details of one recipe\n" +
+                "orders / orders <order ID> -- lists all orders / lists the details of one order\n" +
+                "customer / customer <customer ID/customer lastname> -- lists all customers / lists the details of one customer\n" +
+                "recreate -- recreates the Database --WARNING-- THIS WILL RESET ALL DATA TO ITS DEFAULT VALUES!\n" +
+                "exit -- closes the program\n" +
+                "\n\n");
     }
 
     public void ingredients(){
