622ac1b172b3a94cb41c35e299d3186a3226ff30
JAKOBS-LAPTOP\jakob <jakob.kamper.recke@googlemail.com>
2020-10-01 12:13:45 +0200

    Changed "help" command to reflect changes to the "customer" command

--- src/main/java/utils/Commander.java
+++ src/main/java/utils/Commander.java
@@ -22,7 +22,7 @@ public class Commander {
                 "ingreds / ingreds <ingredient name/ingredient ID> -- list all ingredients / lists the details of one igredient\n" +
                 "recipe / recipe <recipe name/recipe ID> -- lists all recipes / lists the details of one recipe\n" +
                 "orders / orders <order ID> -- lists all orders / lists the details of one order\n" +
-                "customer / customer <customer ID/customer lastname> -- lists all customers / lists the details of one customer\n" +
+                "customer / customer <customer ID/customer lastname firstname> -- lists all customers / lists the details of one customer\n" +
                 "recreate -- recreates the Database --WARNING-- THIS WILL RESET ALL DATA TO ITS DEFAULT VALUES!\n" +
                 "exit -- closes the program\n" +
                 "\n\n");
