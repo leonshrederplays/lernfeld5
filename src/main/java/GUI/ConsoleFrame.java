897f0032fe54e90364cfd5fc105b8785bce31efc
JAKOB-PC\Jakob <jakob.kamper.recke@googlemail.com>
2020-10-20 11:43:43 +0200

    updated ConsoleFrame

--- src/main/java/GUI/ConsoleFrame.java
+++ src/main/java/GUI/ConsoleFrame.java
@@ -1,7 +1,11 @@
 package GUI;
 
+import commands.*;
+import utils.DBUtils;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.Scanner;
 
 public class ConsoleFrame extends BaseFrame {
     private JTextField commandField;
@@ -9,6 +13,7 @@ public class ConsoleFrame extends BaseFrame {
     private JLabel consoleLabel;
     private JLabel commandLabel;
     private JPanel consolePanel;
+    private JButton commandButton;
     @Override
     void initialize() {
         commandField = new JTextField();
@@ -17,11 +22,111 @@ public class ConsoleFrame extends BaseFrame {
         consoleArea.setPreferredSize(new Dimension(1280,640));
         consoleLabel = new JLabel("Console: ");
         commandLabel = new JLabel("Enter the command you want to execute: ");
+        commandButton = new JButton("Execute");
+        commandButton.setPreferredSize(new Dimension(200,100));
+
+        commandButton.addActionListener(ev -> {
+                    String[] command;
+                        command = commandField.getText().split(" ");
+                        switch (command[0]) {
+                            case "help" -> {
+                                Help help = new Help();
+                                help.helper();
+                            }
+                            case "ingreds" -> {
+                                Ingredients ingred = new Ingredients();
+                                try {
+                                    if (command.length > 1) {
+                                        consoleArea.setText(ingred.ingredientDescription(command[1]));
+                                    } else {
+                                        consoleArea.setText(ingred.ingredient().toString());
+                                    }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            case "recipe" -> {
+                                Recipes recipe = new Recipes();
+                                try {
+                                    if (command.length > 1) {
+                                        recipe.recipeDescription(command[1]);
+                                    } else {
+                                        recipe.recipe();
+                                    }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                                System.out.println(" ");
+                            }
+                            case "addrecipe" -> {
+                                AddRecipe addrecipe = new AddRecipe();
+                                System.out.println("You typed addrecipe so now we will create a recipe lets start.");
+                                addrecipe.addRecipeName();
+                            }
+                            case "orders" -> {
+                                Orders order = new Orders();
+                                try {
+                                    if (command.length > 1) {
+                                        order.orderDescription(command[1]);
+                                    } else {
+                                        order.orders();
+                                    }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            case "customers" -> {
+                                Customers cust = new Customers();
+                                cust.passwordManager(command);
+                            }
+                            case "categories" -> {
+                                Categories categ = new Categories();
+                                try {
+                                    if (command.length > 1) {
+                                        categ.categoryRecipes(command[1]);
+                                    } else {
+                                        categ.categories();
+                                    }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            case "allergens" -> {
+                                Allergens allerg = new Allergens();
+                                try {
+                                    if (command.length > 1) {
+                                        allerg.allergenRecipes(command[1]);
+                                    } else {
+                                        allerg.allergens();
+                                    }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            case "recreate" -> {
+                                Recreate rec = new Recreate();
+                                rec.recreate();
+                            }
+                            case "reload" -> {
+                                Reload rel = new Reload();
+                                rel.reload();
+                            }
+                            case "exit" -> {
+                                Shutdown shut = new Shutdown();
+                                shut.shutdown();
+                            }
+                            case "error" -> {
+                                DBUtils dbUtils = new DBUtils();
+                                dbUtils.error();
+                            }
+                            default -> consoleArea.setText("YEET: " + command[0] + " isnt a command type help to get the command list.");
+                        }
+                });
 
         consolePanel = new JPanel();
         consolePanel.setPreferredSize(new Dimension(1280, 720));
         consolePanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-        GridLayout layout = new GridLayout(0, 1);
+        GridLayout layout = new GridLayout(0,2);
         layout.setVgap(20);
         layout.setHgap(10);
         consolePanel.setLayout(layout);
@@ -29,6 +134,8 @@ public class ConsoleFrame extends BaseFrame {
         consolePanel.add(consoleArea);
         consolePanel.add(commandLabel);
         consolePanel.add(commandField);
+        consolePanel.add(new JLabel());
+        consolePanel.add(commandButton);
 
         add(consolePanel);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
--- src/main/java/commands/Ingredients.java
+++ src/main/java/commands/Ingredients.java
@@ -7,35 +7,40 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class Ingredients {
-    public void ingredient() {
+    public String ingredient() {
+        var ref = new Object() {
+            String str;
+        };
         List<IngredientList> list = ConfigInstance.ingredientList;
         list.forEach(ingredient -> {
-            String str ="ID: " + ingredient.getIngredientID()
+            ref.str ="ID: " + ingredient.getIngredientID()
                     + " | Name: " + ingredient.getIngredientName();
-            System.out.println(str);
         });
+        return ref.str;
     }
 
-    public void ingredientDescription(String arg) {
+    public String ingredientDescription(String arg) {
+        var ref = new Object() {
+            String str = null;
+        };
         List<IngredientList> list = ConfigInstance.ingredientList;
         try {
             int id = Integer.parseInt(arg);
             list.stream().filter(ingredient -> new BigDecimal(id).equals(ingredient.getIngredientID())).findAny().ifPresentOrElse(ingredient -> {
-                String str =
+                ref.str =
                         "Properties of the Ingredient: "
                                 + "ID: " + ingredient.getIngredientID() + " | Name: " + ingredient.getIngredientName()
-                                + "\nUnit: " + ingredient.getUnit() 
-                                + "\nNetprice: " + ingredient.getNettoprice() 
-                                + "\nIn Stock: " + ingredient.getAmount() 
-                                + "\nSupplier: " + ingredient.getSupplierID() + " | " + ingredient.getSupplierName() 
-                                + "\nCalories: " + ingredient.getCalorie() 
-                                + "\nCarbohydrates: " + ingredient.getCarbohydrates() 
+                                + "\nUnit: " + ingredient.getUnit()
+                                + "\nNetprice: " + ingredient.getNettoprice()
+                                + "\nIn Stock: " + ingredient.getAmount()
+                                + "\nSupplier: " + ingredient.getSupplierID() + " | " + ingredient.getSupplierName()
+                                + "\nCalories: " + ingredient.getCalorie()
+                                + "\nCarbohydrates: " + ingredient.getCarbohydrates()
                                 + "\nProtein: " + ingredient.getProtein();
-                System.out.println(str);
-            }, () -> System.out.println("The ingredient: " + arg + " does not exist. type ingreds to list all ingredients."));
+            }, () -> ref.str = "The ingredient: " + arg + " does not exist. type ingreds to list all ingredients.");
         } catch (NumberFormatException e) {
             list.stream().filter(ingredient -> arg.toLowerCase().equals(ingredient.getIngredientName().toLowerCase())).findAny().ifPresentOrElse(ingredient -> {
-                String str =
+                ref.str =
                         "Properties of the Ingredient: "
                                 + "\nID: " + ingredient.getIngredientID() + " | Name: " + ingredient.getIngredientName()
                                 + "\nUnit: " + ingredient.getUnit()
@@ -45,9 +50,9 @@ public class Ingredients {
                                 + "\nCalories: " + ingredient.getCalorie() 
                                 + "\nCarbohydrates: " + ingredient.getCarbohydrates() 
                                 + "\nProtein: " + ingredient.getProtein();
-                System.out.println(str);
-            }, () -> System.out.println("The ingredient: " + arg + " does not exist. type ingreds to list all ingredients."));
-        }
+
+            }, () -> ref.str = "The ingredient: " + arg + " does not exist. type ingreds to list all ingredients.");
+        }return ref.str;
     }
 
 }
