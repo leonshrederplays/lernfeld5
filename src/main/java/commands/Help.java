a061bd22d47d4c8532402e48da3cfb5496ff102b
JAKOB-PC\Jakob <jakob.kamper.recke@googlemail.com>
2020-10-21 08:32:37 +0200

    Updated help command

--- src/main/java/commands/Help.java
+++ src/main/java/commands/Help.java
@@ -3,20 +3,24 @@ package commands;
 public class Help {
 
     public void helper() {
+        System.out.println(" ");
         System.out.print("""
-                Here is a list of available commands:
+                      Here is a list of available commands:
 
-                help -- shows a list of all available commands
-                ingreds / ingreds <ingredient name/ingredient ID> -- list all ingredients / lists the details of one igredient
-                recipe / recipe <recipe name/recipe ID> -- lists all recipes / lists the details of one recipe
-                orders / orders <order ID> -- lists all orders / lists the details of one order
-                customers / customers <customer ID/customer lastname firstname> -- lists all customers / lists the details of one customer
-                recreate -- recreates the Database --WARNING-- THIS WILL RESET ALL DATA TO ITS DEFAULT VALUES!
-                reload -- reloads the entire lists of ingredients, orders etc...
-                exit -- closes the program
+                      help -- shows a list of all available commands
+                      ingreds / ingreds <ingredient name/ingredient ID> -- list all ingredients / lists the details of one igredient
+                      recipe / recipe <recipe name/recipe ID> -- lists all recipes / lists the details of one recipe
+                      orders / orders <order ID> -- lists all orders / lists the details of one order
+                      customers / customers <customer ID / customer lastname firstname> -- lists all customers / lists the details of one customer
+                      categories / categories <category ID / category name> -- lists all categories / lists all recipes of one category
+                      addrecipe -- adds a recipe		
+                      buy recipe <recipe ID / recipe name> -- buys a recipe
+                      buy ingred <ingredient ID / ingredient name> -- buys an ingredient
+                      recreate -- recreates the Database --WARNING-- THIS WILL RESET ALL DATA TO ITS DEFAULT VALUES!
+                      reload -- reloads the entire lists of ingredients, orders etc...
+                      exit -- closes the program
 
-
-                """);
+                      """);
     }
 
 }
